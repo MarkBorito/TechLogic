093a6f0a947459ab96469849a2b98b1ec986bebe
MarkBorito <markadrianborito30@gmail.com>
2026-03-27 19:58:10 +0800

    SplashScreen

--- app/src/main/java/com/appdev/techlogic/SplashScreen.java
+++ app/src/main/java/com/appdev/techlogic/SplashScreen.java
@@ -1,6 +1,8 @@
 package com.appdev.techlogic;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +12,12 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+            }
+        }, 2000);
     }
 }
